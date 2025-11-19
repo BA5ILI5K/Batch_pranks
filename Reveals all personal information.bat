0dce3af9d2a85e4aab6c24c9f091179c195336ae
Basilisk <sammympatto@gmail.com>
2025-11-19 18:15:24 +1100

    Add files via upload

---
+++ Reveals all personal information.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Hello, I know your details; your main directory is; %USERPROFILE%
+timeout 5 >nul
+echo And your computers name is %ComputerName%
+timeout 4 >nul
+echo I also know all the ip addresses on your network; I'll list them all below.
+timeout 5 >nul
+arp -a
+timeout 4 >nul
+echo Yes, I know, scary. Ok, Goodnight!
+REM shutdown /s /t 2
